0e07562cd529a9c73189c185488a06a4b987e5bc
Kim Jooeon <joo2theeon@gmail.com>
2017-10-14 17:17:12 +0900

    test: model.field option choice

--- instagram/post/models.py
+++ instagram/post/models.py
@@ -7,8 +7,26 @@ __all__ = (
 
 
 class Post(models.Model):
+    MEDIA_CHOICES = (
+        ('Audio', (
+            ('vinyl', 'Vinyl'),
+            ('cd', 'CD'),
+        )
+         ),
+        ('Video', (
+            ('vhs', 'VHS Tape'),
+            ('dvd', 'DVD'),
+        )
+         ),
+        ('unknown', 'Unknown'),
+    )
     photo = models.ImageField(upload_to='post')
+    text = models.CharField(max_length=50, choices=MEDIA_CHOICES, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def text_display(self):
+        return self.get_text_display()
 
 
 class PostComment(models.Model):
